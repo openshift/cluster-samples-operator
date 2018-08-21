eccdb3ca00baec2b280c2ebb12a7c56edec7f818
gabemontero <gmontero@redhat.com>
2018-08-21 16:22:45 -0400

    pull in client-go into vendor; strip vendors within vendors

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -2,15 +2,21 @@ package stub
 
 import (
 	"context"
+	"io/ioutil"
 
 	"github.com/openshift/cluster-samples-operator/pkg/apis/samplesoperator/v1alpha1"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+
+	//"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
+	//"k8s.io/apimachinery/pkg/runtime/schema"
+	restclient "k8s.io/client-go/rest"
+
+	imagev1client "github.com/openshift/client-go/image/clientset/versioned/typed/image/v1"
+	templatev1client "github.com/openshift/client-go/template/clientset/versioned/typed/template/v1"
 )
 
 func NewHandler() sdk.Handler {
@@ -24,17 +30,37 @@ type Handler struct {
 func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 	switch o := event.Object.(type) {
 	case *v1alpha1.SamplesResource:
-		err := sdk.Create(newbusyBoxPod(o))
+		logrus.Printf("ctrl cfg %#v", o)
+		restconfig, err := getRestConfig()
+		if err != nil {
+			logrus.Errorf("failed to get rest config : %v", err)
+			return err
+		}
+		tempclient, err := getTemplateClient(restconfig)
+		if err != nil {
+			logrus.Errorf("failed to get template client : %v", err)
+			return err
+		}
+		logrus.Printf("template client %#v", tempclient)
+		imageclient, err := getImageClient(restconfig)
+		if err != nil {
+			logrus.Errorf("failed to get image client : %v", err)
+			return err
+		}
+		logrus.Printf("image client %#v", imageclient)
+		templist, err := tempclient.Templates("openshift").List(metav1.ListOptions{})
+		logrus.Printf("template list %#v", templist)
+		/*err := sdk.Create(newbusyBoxPod(o))
 		if err != nil && !errors.IsAlreadyExists(err) {
 			logrus.Errorf("failed to create busybox pod : %v", err)
 			return err
-		}
+		}*/
 	}
 	return nil
 }
 
 // newbusyBoxPod demonstrates how to create a busybox pod
-func newbusyBoxPod(cr *v1alpha1.SamplesResource) *corev1.Pod {
+/*func newbusyBoxPod(cr *v1alpha1.SamplesResource) *corev1.Pod {
 	labels := map[string]string{
 		"app": "busy-box",
 	}
@@ -65,4 +91,23 @@ func newbusyBoxPod(cr *v1alpha1.SamplesResource) *corev1.Pod {
 			},
 		},
 	}
+}*/
+
+func getTemplateClient(restconfig *restclient.Config) (*templatev1client.TemplateV1Client, error) {
+	return templatev1client.NewForConfig(restconfig)
+}
+
+func getImageClient(restconfig *restclient.Config) (*imagev1client.ImageV1Client, error) {
+	return imagev1client.NewForConfig(restconfig)
+}
+
+func getRestConfig() (*restclient.Config, error) {
+	// Build a rest.Config from configuration injected into the Pod by
+	// Kubernetes.  Clients will use the Pod's ServiceAccount principal.
+	return restclient.InClusterConfig()
+}
+
+func getNamespace() string {
+	b, _ := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/" + corev1.ServiceAccountNamespaceKey)
+	return string(b)
 }
