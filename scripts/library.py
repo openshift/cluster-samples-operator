9cb6266998a1efa2c36f4f60fa48405be00e3396
Feny Mehta <fbm3307@gmail.com>
2022-08-04 11:45:16 +0530

    Adding the workflow and create issue python file
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- scripts/library.py
+++ scripts/library.py
@@ -4,23 +4,36 @@ from re import template
 import sys
 from typing import final
 import yaml
+import requests
+import argparse
+from create_issue import *
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+user_input = parser.repoType
+print("Selected Repo Type: ",user_input)
+
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
 
-LIBRARY_FILE='/Users/fmehta/Projects/library/official.yaml'
 
+imageStreamDict = {}
+templateDict = {}
+combinedDict = {}
+testDict = {"test":["fbm3307/test-learn"]}
 
-if not os.path.exists(LIBRARY_FILE):
-    print(f"[ERROR] {LIBRARY_FILE} file does not exist.")
-else:
-    loaddata = open(LIBRARY_FILE).read()
-    filedata = yaml.safe_load(loaddata)
+def load_yaml():
+    global imageStreamDict
+    global templateDict
+    global combinedDict
+
+    LIBRARY_FILE= requests.get("https://github.com/openshift/library/blob/master/official.yaml?raw=true")
+    filedata = yaml.safe_load(LIBRARY_FILE.text)
     githubcontent=filedata["data"]
-    imageStreamDict = {}
-    templateDict = {}
-    combinedDict = {}
+    
     for reponame in githubcontent:
         imagestreamLocationSet = set() #Initialize the locationSet
         if("imagestreams" in githubcontent[reponame]):
@@ -28,8 +41,9 @@ else:
             for ele in githubcontent[reponame]["imagestreams"]:
                 location = ele["location"]
                 temp = (str(location).split("//")[1]).split("/")
-                one,two = temp[1], temp[2]
-                finalUrl = "https://github.com/" + str(one) +"/" + str(two)
+                repo1,repo2 = temp[1], temp[2]
+                #finalUrl = "https://github.com/" + str(repo1) +"/" + str(repo2)
+                finalUrl = str(repo1) + "/" + str(repo2)
                 imagestreamLocationSet.add(finalUrl)
             imageStreamDict[reponame] = list(imagestreamLocationSet)
         templateLocationSet = set() #Re-Initialize the location list
@@ -38,21 +52,32 @@ else:
             for ele in githubcontent[reponame]["templates"]:
                 location = ele["location"]
                 temp = (str(location).split("//")[1]).split("/")
-                one,two = temp[1], temp[2]
-                finalUrl = "https://github.com/" + str(one) +"/" + str(two)
+                repo1,repo2 = temp[1], temp[2]
+                #finalUrl = "https://github.com/" + str(repo1) +"/" + str(repo2)
+                finalUrl = str(repo1) + "/" + str(repo2)
                 templateLocationSet.add(finalUrl)
             templateDict[reponame] = list(templateLocationSet)
         imagestreamLocationSet.update(templateLocationSet)
         combinedDict[reponame] = list(imagestreamLocationSet)
-        print(reponame)
-        if("imagestreams" in githubcontent[reponame]):
-            print("imagestream", imageStreamDict[reponame])
-        if("templates" in githubcontent[reponame]):
-            print("templates", templateDict[reponame])
-        print("combined", combinedDict[reponame])
-        #input("Press")
-    print("Out of loop)")
-    input("Press")
-    print("Template", templateDict)
-    print("imageStreams", imageStreamDict)
-    print("combined", combinedDict)
+    return True
+
+if(load_yaml()):
+    targetDict = {}
+    if(user_input == "all"):
+        targetDict = combinedDict
+    elif(user_input == "templates"):
+        targetDict = templateDict
+    elif(user_input == "imagestreams"):
+        targetDict = imageStreamDict
+    elif(user_input == "test"):
+        targetDict = testDict
+    else:
+        print("Invalid input")
+        exit()
+    for repoName in targetDict.keys():
+        repoList = targetDict[repoName]
+        for repo in repoList:
+            create_an_issue(title="sample issue",description="sample description", repo=str(repo))
+            input("Check for the issue")
+else:
+    print("Error")
