692cffbbda2faa8802328641c7b4887eace008c7
Feny Mehta <fbm3307@gmail.com>
2022-08-23 16:52:36 +0530

    number#9
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- scripts/library.py
+++ scripts/library.py
@@ -82,6 +82,7 @@ else:
     exit()
 for repoName in targetDict.keys():
     repoList = targetDict[repoName]
+    print("Creating Repo : ", repoList)
     for repo in repoList:
         create_an_issue(title="sample issue",description="sample description", repo=str(repo))
         print("created the issues in target repos")
